6bcc8d80aae43fd6e9afe92334bb905ec8162c3d
JasonIsaacHuang <jasonisaachuang@gmail.com>
2017-02-15 22:35:12 +1100

    Introduce location functionality in the form of a service. Hooked up the fragment_locate fab to trigger a location request and display the lat,long in a toast message.

---
+++ app/src/main/java/com/petrolpatrol/petrolpatrol/util/Constants.java
@@ -0,0 +1,13 @@
+package com.petrolpatrol.petrolpatrol.util;
+
+/**
+ * Created by jason on 14/02/17.
+ */
+public class Constants {
+
+    // unique app wide IDs to identify a permission request
+    public static final int PERMISSION_REQUEST_ACCESS_LOCATION = 1;
+
+    // intent identifiers for broadcast receivers
+    public static final String NEW_LOCATION_AVAILABLE = "new location available";
+}
